cab3310f86b3c4dd17055575a829abb9c312bce1
ishanttech <ishant@techwatts.com>
2015-05-15 10:27:14 +0530

    Shopmapviewcontroller map working

---
+++ ShopMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShopMapViewController.h
+//  Kushme
+//
+//  Created by Test on 15/05/15.
+//  Copyright (c) 2015 Ishant Tiwari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShopMapViewController : UIViewController
+
+@end
